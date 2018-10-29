f25458fbd231767c60325de31b82e509df77d191
Alexy <alexandra.fina@yahoo.com>
2018-10-29 16:18:21 +0000

    Initialise Puzzle Game react app

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Enzyme, {shallow, mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
+ 
+Enzyme.configure({ adapter: new Adapter() });
+
 import App from './App';
 
 it('renders without crashing', () => {
@@ -7,3 +13,11 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders App Component', () => {
+  const tree = renderer.create(
+    <App />
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+})
